c3323ecb19d5ef9ccf71ee3f0a200df0b6fd6210
Yusuke Ito <novi.mad@gmail.com>
2015-04-05 19:38:46 +0900

    プロジェクトを作成

---
+++ ProconApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ProconApp
+//
+//  Created by Yusuke on 2015/04/05.
+//  Copyright (c) 2015年 Procon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
