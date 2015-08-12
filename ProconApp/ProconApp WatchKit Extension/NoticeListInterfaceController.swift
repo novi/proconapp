f293ca1f45fc5fe34075ada942f7c69267531f8f
hanamiju <watashi.wa.hanamiju@gmail.com>
2015-08-13 01:21:19 +0900

    watchのStoryBoard更新 watch系のファイル追加

---
+++ ProconApp/ProconApp WatchKit Extension/NoticeListInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  NoticaListInterfaceController.swift
+//  ProconApp
+//
+//  Created by Goodpatch on 2015/08/13.
+//  Copyright (c) 2015年 Procon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class NoticeListInterfaceController: InterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
